f8f7f68a53ff921ea7c9f712fdec47a0b35e33fd
dstelter92 <dstelter@bu.edu>
2017-10-06 10:31:06 -0400

    Updated walkers 2 replica, now ~5x faster

--- tools/get-hist-enthalpies.py
+++ tools/get-hist-enthalpies.py
@@ -22,101 +22,129 @@ NumReplica = int(sys.argv[1])
 start = int(sys.argv[2])
 stop = int(sys.argv[3])
 
-# Per-run info, requires manual edits for now...
-steps = 50000
-exchange = 1000
-thermo = 50
-
 # Logfile info, requires manual edits for now...
-header = 111
+header = 139
 footer = 29
-##################
 
-length = steps/thermo
-length_walk = steps/exchange
-ratio = exchange/thermo
+# Output walkers too?
+walker = 1
+##################
+nfiles = (stop - start) # Number of files
 
 print "Gather enthalpies from log files, follow exchanges, save data for each replica instead of per walker"
 
-size = ((length)*(stop-start),3*NumReplica+1)
-size_walk = ((length)*(stop-start),NumReplica+1)
-full_data = empty(size)
-full_data[:] = NAN
-# Format: step, indx1, indx2,... indxN, walk1, walk2,...walkN, rep1, rep2,...repN
-
 # Save Exchanges
 loop = 1.0 / (stop - start)
+tmp_Walk = []
 print "Read exchanges..."
 for i in range(stop-start):
     wname = '../log/log.lammps' + '-' + str(start+i)
-    Walk = genfromtxt(wname, skip_header=3, skip_footer=1)
+    Walk = genfromtxt(wname,dtype=int,skip_header=3,skip_footer=1)
+    tmp_Walk.append(Walk)
     sys.stdout.write("  %d%% completed\r" % int(loop*(i+1)*100))
     sys.stdout.flush()
-    for j in range(NumReplica):
-        for k in range(length):
-            indx = k+(i*length)
-            if (exchange == steps):
-                full_data[:,j+1][indx] = int(Walk[j+1])
-            else:
-                full_data[:,j+1][indx] = int(Walk[:,j+1][k/ratio])
+tot_Walk = array(tmp_Walk)
 print "\n"
 
-# Save Walkers
+# Define useful stuff from Walk array
+lwalk = len(tot_Walk[:,0])            # Number of files from exchanges
+walkers = len(tot_Walk[0][0])-1       # Number of walkers/replicas
+exchange = tot_Walk[0][1][0] - tot_Walk[0][0][0] # Exchange freq
+steps = tot_Walk[0][-1][0] + exchange # Number of steps
+nexchange = len(tot_Walk[0])          # Number of exchanges
+
+INDX = reshape(tot_Walk,(nfiles*nexchange,NumReplica+1))
+
+if (NumReplica != walkers):
+  print "Err. Number of walkers in lammps output does not equal number of replicas specified"
+  sys.exit()
+
+# Save Walker data
 loop = 1.0 / NumReplica
 loop2 = (loop * 1.0) / (stop - start)
-print "Input data for ", NumReplica, " walkers..."
+tmp_Hist = []
+print "Input data for", NumReplica, "walkers..."
 for i in range(NumReplica):
-    #print "\n===--- Walker", i, "---==="
-    for j in range(stop-start):
-        hname = '../' + str(i) + '/log.lammps.' + str(i) + '-' + str(start+j)
-        Hist = genfromtxt(hname, skip_header=header, skip_footer=footer, usecols = (0,2))
+  walk_Hist = []
+  for j in range(stop-start):
+    # Read data
+    hname = '../' + str(i) + '/log.lammps.' + str(i) + '-' + str(start+j)
+    Hist = genfromtxt(hname,skip_header=header,skip_footer=footer,usecols=(0,2))
+    walk_Hist.append(array(Hist))
 
-        # Error checking
-        if (Hist[0][0] != 0):
-          print "Err. Header value is incorrect", Hist[0][0]
-          sys.exit()
+    # Write % completion
+    percent = (j+1)*loop2 + (i)*loop
+    sys.stdout.write("  (%.1f%%) Walker %d: Data index, %d Energy: min=%.3f max=%.3f\r" % (percent*100,i,j+start,amin(Hist[:,1]),amax(Hist[:,1])))
+    sys.stdout.flush()
 
-        if (Hist[-1][0] != steps-thermo):
-          print "Err. Footer value is incorrect", Hist[-1][0]
-          sys.exit()
-
-        #sys.stdout.write("Walker %d, Data index: %d\n" % (i,j+start))
-        #sys.stdout.write("  Step: start=%d end=%d\n" % (Hist[0][0],Hist[-1][0]))
-        percent = (j+1)*loop2 + (i)*loop
-        sys.stdout.write("  (%.1f%%) Walker %d: Data index, %d Energy: min=%.3f max=%.3f\r" % (percent*100,i,j+start,amin(Hist[:,1]),amax(Hist[:,1])))
-        sys.stdout.flush()
-
-        for k in range(length):
-        # Load enthalpy data in global array
-            indx = k+(j*length)
-            full_data[:,0][indx] = Hist[:,0][k] + (j*steps)
-            full_data[:,i+NumReplica+1][indx] = Hist[:,1][k]
+    if (Hist[0][0] != 0):
+      print "\n"
+      print "Err. Header value is incorrect", Hist[0][0]
+      sys.exit()
+    if (Hist[-1][0] >= steps):
+      print "\n"
+      print "Err. Footer value is incorrect", Hist[-1][0], steps
+      sys.exit()
+  tmp_Hist.append(array(walk_Hist))
+tot_Hist = array(tmp_Hist)
 print "\n"
+
+# Define some useful things from Hist arrays
+nthermo = len(tot_Hist[0][0])             # Number of thermo outputs
+thermo = steps / nthermo                  # Thermo freq
+ratio = nthermo / nexchange               # Ratio of thermo / exchange data
+numwalkers = len(tot_Hist)                # Number of walkers read in
+lhist = len(tot_Hist[0])                  # Number of files read in
+numdata = nthermo * numwalkers * lhist    # Total # of data
+
+if ((nfiles != lwalk) or (lhist != nfiles) or (lhist != lwalk)):
+  print "Err. Not all data read in correctly"
+  sys.exit()
+
+if (NumReplica != numwalkers):
+  print "Err. Number of walker data read in is different than what specified"
+  sys.exit()
 
 # Save Replicas
 print "Get replicas..."
-for rep in range(NumReplica):
-    sys.stdout.write("  %d%% completed\r" % int(loop*(rep+1)*100))
-    sys.stdout.flush()
-    for indx in range(len(full_data[:,0])):
-        for j in range(NumReplica):
-            if (full_data[:,j+1][indx] == rep):
-                full_data[:,rep+(2*NumReplica)+1][indx] = full_data[:,j+NumReplica+1][indx]
+f_replica_data = []
+loop = 1.0 / nfiles
+loop2 = (loop * 1.0) / (nexchange)
+loop3 = (loop2 * 1.0) / (NumReplica)
+# put all data here
+replica_data = empty((NumReplica,nfiles*nthermo))
+walker_data = empty((NumReplica,nfiles*nthermo))
+# Start loop
+for file in range(nfiles): # Loop over files
+  warg = tot_Walk[file][:,1:] # walkers for each step of file
+  wstep = tot_Walk[file][:,0] # walker steps for this file
+  for frame in range(nexchange): # Loop over num exchanges in each file
+    wlist = warg[frame]
+    s = wstep[frame] + file*steps
+    i = frame*ratio
+    indx = frame*ratio + file*nthermo
+    for walk in range(NumReplica): # Loop over each walker, assign to replica
+      # Output progress
+      percent = (frame)*loop2 + (file)*loop + (walk+1)*loop3
+      sys.stdout.write("  %.1f%% completed\r" % (percent*100))
+      sys.stdout.flush()
+      this_walker = wlist[walk] # walker "walk" belongs to replica "this_walker"
+      hstep = tot_Hist[walk][file][:,0][i:i+ratio] # enthalpy steps with same ex
+      hvalues = tot_Hist[walk][file][:,1][i:i+ratio] # enthalpies with same ex
+      replica_data[this_walker][indx:indx+ratio] = hvalues
+      if (walker):
+        walker_data[walk][indx:indx+ratio] = hvalues
 print "\n"
 
-#savetxt('full_replica_data.dat', full_data)
-walkers = empty(size_walk, dtype=int)
 print "Saving outputs..."
 for rep in range(NumReplica):
     Rname = 'replica-' + str(rep) + '_' + str(start) + '-' + str(stop) + '.dat'
     Wname = 'walker-' + str(rep) + '_' + str(start) + '-' + str(stop) + '.dat'
-    savetxt(Rname, full_data[:, (rep+(2*NumReplica)+1)])
-    savetxt(Wname, full_data[:, (rep+NumReplica+1)])
-    walkers[:,rep+1] = full_data[:,rep+1].astype(int)
+    savetxt(Rname, replica_data[rep])
+    savetxt(Wname, walker_data[rep])
 
-walkers[:,0] = full_data[:,0].astype(int)
 print "Saving exchanges..."
-savetxt('exchange_list.dat', walkers, fmt='%d')
+savetxt('exchange_list.dat', INDX[:,1:], fmt='%d')
 
 print "Done!"
 sys.exit()
